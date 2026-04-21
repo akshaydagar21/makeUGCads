f4bb3773760c3b65d5e5238f07f7c1ef8d25433b
akshaydagar21 <dagarakshay21@gmail.com>
2026-04-22 03:33:12 +0530

    feat(ad): add page to display user projects and UGC

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -2,13 +2,24 @@ import type React from "react"
 import type { Project } from "../types"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
-import { Loader2Icon } from "lucide-react";
+import { EllipsisIcon, ImageIcon, Loader2Icon, PlaySquareIcon, Share2Icon, Trash2Icon } from "lucide-react";
+import { GhostButton, PrimaryButton } from "./Buttons";
 
 const ProjectCard = ({gen, setGenerations, forCommunity = false}: {gen: Project,
 setGenerations: React.Dispatch<React.SetStateAction<Project[]>>, forCommunity?: boolean}) => {
 
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleDelete = async (id: string)=>{
+    const confirm = window.confirm('Are you sure you want to delete this project?');
+    if(!confirm) return;
+    console.log(id)
+  }
+
+    const togglePublish = async (projectId: string)=>{
+        console.log(projectId)
+    }
 
   return (
     <div key={gen.id} className="mb-4 break-inside-avoid">
@@ -52,6 +63,45 @@ setGenerations: React.Dispatch<React.SetStateAction<Project[]>>, forCommunity?: 
                     </span>
                 )}
             </div>
+
+            {/* action menu for my generations only */}
+            {!forCommunity && (
+                <div 
+                onMouseDownCapture={()=>{setMenuOpen(true)}}
+                onMouseLeave={()=>{setMenuOpen(false)}}
+                className="absolute right-3 top-3 sm:opacity-0 group-hover:opacity-100 transition flex items-center gap-2">
+                    <div className="absolute top-3 right-3">
+                    <EllipsisIcon className="ml-auto bg-black/10 rounded-full p-1 size-7" />
+                    </div>
+                    <div className="flex flex-col items-end w-32 text-sm">
+                        <ul className={`text-xs ${menuOpen ? 'block' : 'hidden'} overflow-hidden right-0 peer-focus:block hover:block w-40 bg-black/50 backdrop-blur text-white border border-gray-500/50 rounded-lg shadow-md mt-2 py-1 z-10`}>
+                            
+                            {gen.generatedImage && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                            <ImageIcon size={14} /> Download Image
+                            </a>}
+
+                            {gen.generatedVideo && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                                <PlaySquareIcon size={14} /> Download Video
+                            </a>}
+
+                            {(gen.generatedVideo || gen.generatedImage) && <button
+                                onClick={() => navigator.share({url: gen.generatedVideo || gen.generatedImage, title: gen.productName, text: gen.productDescription})}
+                                className="w-full flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                                <Share2Icon size={14}/> Share
+                            </button>}
+
+                            <button onClick={() => handleDelete(gen.id)}
+                                className="w-full flex gap-2 items-center px-4 py-2 hover:bg-red-950/10 text-red-400 cursor-pointer">
+                                <Trash2Icon size={14}/> Delete
+                            </button>
+                            
+                        </ul>
+                    </div>
+
+                </div>
+            )}
+            
+
 
             {/* source images */}
             <div className="absolute right-3 bottom-3">
@@ -107,6 +157,20 @@ setGenerations: React.Dispatch<React.SetStateAction<Project[]>>, forCommunity?: 
             <div className="mt-3">
                 <div className="text-xs text-gray-300 ">{gen.userPrompt}</div>
             </div>
+            )}
+
+            {/* buttons */}
+            {!forCommunity && (
+                <div className="mt-4 grid grid-cols-2 gap-3">
+                    <GhostButton className="text-xs justify-center"
+                    onClick={() => {navigate(`/result/${gen.id}`); scrollTo(0,0)}}>
+                    View Details
+                    </GhostButton>
+                    <PrimaryButton onClick={() => togglePublish(gen.id)}
+                    className="rounded-md">
+                    {gen.isPublished ? 'Unpublish' : 'Publish'}
+                    </PrimaryButton>
+                </div>
             )}
 
         </div>
