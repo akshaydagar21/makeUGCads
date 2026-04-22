e7080594cb84dd67d208efb75ea426f83efab210
akshaydagar21 <dagarakshay21@gmail.com>
2026-04-23 04:39:38 +0530

    chore(db): setup database with Prisma

---
+++ server/server.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerk";
+
+const app = express();
+
+// Middleware
+app.use(cors())
+
+app.post('/api/clerk', express.raw({type: 'application/json'}) ,clerkWebhooks)
+
+app.use(express.json());
+
+app.use(clerkMiddleware())
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
